ebf137e7fd1703cc29e217fc03af1dce48d732fd
TheBoxer <theboxercz@gmail.com>
2011-06-13 19:53:16 +0200

    Change forced query to newQuery

--- core/components/bookit/processors/mgr/bookit/openschedule/addOpenschedule.php
+++ core/components/bookit/processors/mgr/bookit/openschedule/addOpenschedule.php
@@ -15,90 +15,34 @@ if(empty($scriptProperties['openTo'])) {
     $modx->error->addField('openTo',$modx->lexicon('bookit.error_no_opentp'));
 }
 
-/*
+
 $e = $modx->newQuery('BookItemsOpen');
 
 $e->where(
+    array(
         array(
-            'idItem' => $scriptProperties['idItem'],
-            'openDay' => $scriptProperties['openDay']
-   )
-);
-$e->andCondition(
+            array(
                 array(
                     'openFrom:<=' => $scriptProperties['openFrom'],
                     'openTo:>=' => $scriptProperties['openFrom']
-                ), null, 1);
-$e->andCondition(
+                ),
                 array(
                     'OR:openFrom:<=' => $scriptProperties['openTo'],
                     'openTo:>=' => $scriptProperties['openTo']
-                ), null, 1);
-                
-                $e->prepare();
-                $modx->fire->warn($e->toSQL(), "sql");
-                return;
-
-
-$e->where(
-    array(
-        'idItem' => $scriptProperties['idItem'],
-        'openDay' => $scriptProperties['openDay'],
-        array(
-            array(
-                'openFrom:<=' => $scriptProperties['openFrom'],
-                'openTo:>=' => $scriptProperties['openFrom']
+                )
             ),
             array(
-                'OR:openFrom:<=' => $scriptProperties['openTo'],
-                'openTo:>=' => $scriptProperties['openTo']
-            )
-        )
+                    'OR:openFrom:>=' => $scriptProperties['openFrom'],
+                    'openTo:<=' => $scriptProperties['openTo']
+                )
+        ),
+        'idItem' => $scriptProperties['idItem'],
+        'openDay' => $scriptProperties['openDay'],
     )
 );
 
-$e->prepare();
-//$modx->fire->warn($e->toSQL(), "SQL");
- */
-//$count = $modx->getCount('BookItemsOpen',$e);
+$count = $modx->getCount('BookItemsOpen',$e);
 
-$query = "SELECT `BookItemsOpen`.`id` AS `BookItemsOpen_id`, `BookItemsOpen`.`idItem` AS `BookItemsOpen_idItem`, `BookItemsOpen`.`openDay` AS `BookItemsOpen_openDay`, `BookItemsOpen`.`openFrom` AS `BookItemsOpen_openFrom`, `BookItemsOpen`.`openTo` AS `BookItemsOpen_openTo` FROM `modx_book_items_open` AS `BookItemsOpen` WHERE  
-    `BookItemsOpen`.`idItem` = ".$scriptProperties['idItem']." AND `BookItemsOpen`.`openDay` = ".$scriptProperties['openDay']." 
-    AND 
-    (
-        ( 
-            (
-                ( 
-                    `BookItemsOpen`.`openFrom` <= '".$scriptProperties['openFrom']."' 
-                    AND 
-                    `BookItemsOpen`.`openTo` >= '".$scriptProperties['openFrom']."' 
-                ) 
-                OR 
-                ( 
-                    `BookItemsOpen`.`openFrom` <= '".$scriptProperties['openTo']."' 
-                    AND 
-                    `BookItemsOpen`.`openTo` >= '".$scriptProperties['openTo']."' 
-                ) 
-            )
-        ) 
-        OR 
-        (
-            `BookItemsOpen`.`openFrom` >= '".$scriptProperties['openFrom']."' 
-            AND 
-            `BookItemsOpen`.`openTo` <= '".$scriptProperties['openTo']."'
-        )
-    )  ";
-
-//$modx->fire->warn($qeury, "query");
-
-$stmt= $modx->query($query);
-$stmt->execute();
-$results= $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-
-
-$count = count($results);
-                                       
 
 if($count != 0){
     $modx->error->addField('openDay',$modx->lexicon('bookit.error_schedule_exists'));
